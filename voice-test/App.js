52a437a23038e995a44241c043343a034aa2f6c9
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-09-16 02:53:43 +0530

    changes

--- voice-test/App.js
+++ voice-test/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableHighlight,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableHighlight,
 } from "react-native";
 
 // import Voice
@@ -15,71 +15,77 @@ import Voice from "react-native-voice";
 const propTypes = {};
 
 const defaultProps = {};
+const name = "";
 
 export default class App extends React.Component {
-  // constructor
-  constructor(props) {
-    super(props);
-    Voice.onSpeechResults = (res) => {
-      const name = JSON.stringify(res);
-      handleCheck(name);
-    };
-  }
-
-  handleCheck(name) {
-    if (name === "call car") {
-      handleCar();
-    } else if (name === "call bike") {
-      handleBike();
-    } else if (name === "call test") {
-      handleTest();
+    // constructor
+    constructor(props) {
+        super(props);
+        Voice.onSpeechResults = (res) => {
+            this.name = JSON.stringify(res);
+            handleCheck(name);
+        };
     }
-  }
 
-  handleCar() {
-    alert("Car Function Called");
-  }
+    handleCheck(name) {
+        if (name === "call car") {
+            handleCar();
+        } else if (name === "call bike") {
+            handleBike();
+        } else if (name === "call test") {
+            handleTest();
+        }
+    }
 
-  handleBike() {
-    alert("Bike Function Called");
-  }
+    handleCar() {
+        alert("Car Function Called");
+    }
 
-  handleTest() {
-    alert("Test Function Called");
-  }
-  // Voice.start("en-us")
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.container}>
-          <TouchableHighlight onPress={() => handleCar()}>
-            <Image
-              style={styles.imageButton}
-              source={{
-                uri: "https://raw.githubusercontent.com/AboutReact/sampleresource/master/microphone.png",
-              }}
-            />
-          </TouchableHighlight>
-        </View>
-      </SafeAreaView>
-    );
-  }
+    handleBike() {
+        alert("Bike Function Called");
+    }
+
+    handleTest() {
+            alert("Test Function Called");
+        }
+        // Voice.start("en-us")
+    render() {
+        return ( <
+            SafeAreaView style = { styles.container } >
+            <
+            View style = { styles.container } >
+            <
+            TouchableHighlight onPress = {
+                () => handleCar() } >
+            <
+            Image style = { styles.imageButton }
+            source = {
+                {
+                    uri: "https://raw.githubusercontent.com/AboutReact/sampleresource/master/microphone.png",
+                }
+            }
+            />{" "} <
+            /TouchableHighlight>{" "} <
+            /View>{" "} <
+            /SafeAreaView>
+        );
+    }
 }
 
 App.propTypes = propTypes;
 App.defaultProps = defaultProps;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    padding: 5,
-    paddingTop: 200,
-  },
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        padding: 5,
+        paddingTop: 200,
+    },
 
-  imageButton: {
-    width: 50,
-    height: 50,
-  },
+    imageButton: {
+        width: 50,
+        height: 50,
+    },
 });
